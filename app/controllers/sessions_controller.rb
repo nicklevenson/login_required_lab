6c9d5f661f798480e6072587ef6838714645a8bf
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-08 13:33:24 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+ 
+  def new
+  end
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/show'
+    else
+      redirect_to '/login'
+    end
+  end
+  def destroy
+    session.delete :name
+  end
+
+  
+
+
+end
